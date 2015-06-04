a2e5cda09baa7e9dc11d7c5fa36b9b086631f0f5
Yongliang Zhan <yzhan94@gmail.com>
2015-06-04 05:00:51 +0000

    Base de datos

---
+++ models/models.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite
+var sequelize = new Sequelize(null, null, null, {
+  dialect: "sqlite",
+  storage: "quiz.sqlite"
+});
+
+// Importar la definicion de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz; // exportar la definicion de la tabla Quiz
+
+// sequelize.sync() crea e inicializa la tabla de preguntas en la DB
+sequelize.sync().success(function() {
+  // success(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().success(function(count) {
+    if (count === 0) { // la tabla se inicializa solo si esta vacia
+      Quiz.create({
+        pregunta: 'Capital de Italia',
+        respuesta: 'Roma',
+      }).success(function() {
+        console.log('Base de datos inicializada');
+      });
+    }
+  });
+});
